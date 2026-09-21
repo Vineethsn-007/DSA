ce5b1433db0ef43cc9ad974f41e3c30d8af55a95
Vineeth S N <4pa23ic054@pace.edu.in>
2026-09-21 23:47:43 +0530

    ✅ Peak Index in a Mountain Array | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 63.6 MB | 2026-09-21

---
+++ leetcode/medium/0852-peak-index-in-a-mountain-array/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int st=1,end=size(arr)-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]) return mid;
+            if(arr[mid-1]<arr[mid]) st=mid+1;
+            else  end=mid-1; 
+        }
+        return -1;
+    }
+};
